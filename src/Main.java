79be6dae6e72a61862157323c3a17f10509c0578
ulyana <bsergeybabak2000@gmail.com>
2023-02-27 13:25:17 +0300

    третий коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+
+    }
+    public static void task1(){
         int age = 18;
         if (age >= 18) {
             System.out.println("Человек соверешеннолетний");
@@ -9,5 +15,24 @@ public class Main {
             System.out.println();
         }
     }
+    public static void task2(){
+        int tt = 4;
+        if (tt < 5){
+            System.out.println("На улице холодно нужно надеть шапку");
+        }
+        if (tt > 5){
+            System.out.println("Сегодня тепло, можно идти без шапки");
+        }
+        }
+        public static void task3(){
+        int speed1 = 75;
+        if (speed1 >= 60){
+            System.out.println("Если скорость " + speed1 + " ,придёться заплатить штраф");
+        }
+        int speed2 = 55;
+        if (speed2 <= 60){
+            System.out.println("Если скорость " + speed2 + " ,можно ехать спокойно");
+        }
+        }
+    }
 
-}
