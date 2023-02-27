47b2af34325eb9dff7f500646d80ac7a467452b7
ulyana <bsergeybabak2000@gmail.com>
2023-02-27 12:44:56 +0300

    второй непонятный коммит

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Человек соверешеннолетний");
+            if (age < 18) {
+                System.out.println("То надо дождать совершеннолетия");
+            }
+            System.out.println();
+        }
+    }
+
+}
